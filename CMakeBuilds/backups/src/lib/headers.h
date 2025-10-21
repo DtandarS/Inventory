2eba54cc6f25c04a7b452d7c6da485157874d418
DtandarS <tom15naksutti@gmail.com>
2025-10-21 15:08:42 +0300

    moved to XCode to continue coding.
    Still using neovim but will be using xcode for primarily to export code to the phone and compile.
    haven't forgot about cmake through. WIll be making a cmake config that can compile to both ios and android.

---
+++ CMakeBuilds/backups/src/lib/headers.h
@@ -0,0 +1,20 @@
+/*
+
+   This is the meant to connect multiple header files to make things easier
+
+*/
+
+#ifndef HEADER_H
+#define HEADER_H
+
+
+#include "window.h"
+#include <SDL3/SDL_system.h>
+
+
+
+
+
+
+#endif /*HEADER_H*/
+
