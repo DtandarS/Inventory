ea90cfdb193ca9149604746035ab869afd3210fc
DtandarS <tom15naksutti@gmail.com>
2026-01-26 11:19:00 +0200

    Update storage.h
    I have no clue what I am doing.
    
    I know I need to create a table of content first in order to create object with specific information that goes into that table. Then we can extract info from the table with an index or simple function call.
    
    But truthfully. I have no damn clue what I doing right now.

--- the-inventory/code/headers/storage.h
+++ the-inventory/code/headers/storage.h
@@ -2,6 +2,8 @@
 
 
 #include <master.h>
+#include <string>
+#include <cstring>
 using namespace std;
 
 static void msg(char *msg)
@@ -11,19 +13,12 @@ static void msg(char *msg)
 
 struct Storage
 {
-  //  Still new to vectors but my idea is to create a cluster table. When we access the first index then we will check the first index of all vectors but if we want a different name then we will first go through the vector and once we find it. We will go through that column.
-  vector<string> names;
-  vector<int> amount;
-  vector<int> price;
-
-  //  These are some basic boolean values right now. Meant to be used as simple on off switch to tell like "Your booze is almost out" notifications
-  bool inStock = false;
-  bool wants_add = false;
-  bool wants_remove = false;
-
-  //  Count-up() is mean to be used as calculation and validation function. This is nothing crazy.
-  //  Same as Count_up(). Count_down() simply does the calculation function and validates the amounts.
-  int count_up();
-  int count_down();
+  char name;
+  int amount;
+  int total_amount;
+  int meow;
 };
+
+
+
 
